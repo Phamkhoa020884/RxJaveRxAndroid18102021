71c4bdebd5e2a61bffb8cfc5ff70bc87eafc4571
khoa <hatgiong@gmail.com>
2022-01-12 18:13:18 +0700

    up

--- app/src/main/java/com/example/rxjaverxandroid18102021/MainActivity.java
+++ app/src/main/java/com/example/rxjaverxandroid18102021/MainActivity.java
@@ -14,36 +14,39 @@ import java.util.concurrent.Future;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        Observable<String> stringObservable = Observable.just("Teo", "Ti", "Tun", "Tuan", "Hoa", "Lan", "Cuc");
 
+        stringObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
 
-        Future<String> future1 = executorService.submit(new Callable<String>() {
-            @Override
-            public String call() throws Exception {
-                Thread.sleep(4000);
-                return "hello";
-            }
-        });
+                    }
 
-        Future<String> future2 = executorService.submit(new Callable<String>() {
-            @Override
-            public String call() throws Exception {
-                return "abc";
-            }
-        });
-        try {
-            Log.d("BBB",future1.get());
-            Log.d("BBB",future2.get());
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+                    @Override
+                    public void onNext(@NonNull String s) {
+                        Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
+
 
 }
